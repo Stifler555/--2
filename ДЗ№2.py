e226ef009d8d4865647e2c44062645c8e904ca0e
Stifler555 <94130392+Stifler555@users.noreply.github.com>
2022-02-15 08:32:09 +0300

    Update ДЗ№2.py

--- ДЗ№2.py
+++ ДЗ№2.py
@@ -56,17 +56,10 @@ for season, months in year_dict.items():
 Строки необходимо пронумеровать. Если в слово длинное,
 выводить только первые 10 букв в слове.
 
-my_str = input("введите строку ")
-my_word = []
-num = 1
-for el in range(my_str.count(' ') + 1):
-    my_word = my_str.split()
-    if len(str(my_word)) <= 10:
-        print(f" {num} {my_word [el]}")
-        num += 1
-    else:
-        print(f" {num} {my_word [el] [0:10]}")
-        num += 1
+words = input("Введите строку из нескольких слов >>> ").split()
+
+for idx, value in enumerate(words, start=1):
+    print(f"{idx}. {value[:10]}")
 
 
 
