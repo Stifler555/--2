a9c360b8f520da6c5239d1977fc1a25745387717
Stifler555 <94130392+Stifler555@users.noreply.github.com>
2022-02-15 08:39:48 +0300

    Update ДЗ№2.py

--- ДЗ№2.py
+++ ДЗ№2.py
@@ -76,22 +76,19 @@ for idx, value in enumerate(words, start=1):
 Набор натуральных чисел можно задать непосредственно в коде,
 например, my_list = [7, 5, 3, 3, 2].
 
-my_list = [7, 5, 3, 3, 2]
-print(f"Рейтинг - {my_list}")
-digit = int(input("Введите число (111 - выход) "))
-while digit != 111:
-    for el in range(len(my_list)):
-        if my_list[el] == digit:
-            my_list.insert(el + 1, digit)
-            break
-        elif my_list[0] < digit:
-            my_list.insert(0, digit)
-        elif my_list[-1] > digit:
-            my_list.append(digit)
-        elif my_list[el] > digit and my_list[el + 1] < digit:
-            my_list.insert(el + 1, digit)
-    print(f"текущий список - {my_list}")
-    digit = int(input("Введите число "))
+rating = [9, 8, 8, 7, 6, 6, 6, 5, 3, 2, 1]
+
+while True:
+    try:
+        prit(f"Рейтинг = {rating}")
+        user_rate = int(input("Введите новый рейтинг >>> "))
+        rating.append(user_rate)
+        rating.sort(reverse=True)
+        print(rating)
+    except ValueError:
+        print("Неверное число")
+    except KeyboardInterrupt:
+        exit()
 
 
 
